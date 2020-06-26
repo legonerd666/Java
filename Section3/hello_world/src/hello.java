dd13998eb3f49ee173180e7306edaf886a6affe8
Liam Chipman <chipman.lian@gmail.com>
2020-06-25 20:58:43 -0600

    Finished Hello World
    Finished my hello world project

--- Section3/hello_world/src/hello.java
+++ Section3/hello_world/src/hello.java
@@ -1,7 +1,18 @@
 public class hello {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, Liam!");
+
+        int my_first_number = (10 + 5) + (2 * 10);
+        int my_second_number = 12;
+        int my_third_number = my_first_number * 2;
+        int my_total = my_first_number + my_second_number + my_third_number;
+        int my_last_one = 1000 - my_total;
+
+        System.out.println(my_total);
+        System.out.println(my_last_one);
+
     }
 
 }
