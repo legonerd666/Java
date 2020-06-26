eb2ee2dc9497f480fa59a6e9d4a4eaf0842f2651
Liam Chipman <chipman.lian@gmail.com>
2020-06-25 20:59:12 -0600

    byte int short long
    finished my byte short int long project

---
+++ Section3/byte_short_int_long/src/liam/chipman/Main.java
@@ -0,0 +1,31 @@
+package liam.chipman;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int my_value = 10000;
+	    int my_min_int_value = Integer.MIN_VALUE;
+        int my_max_int_value = Integer.MAX_VALUE;
+
+        System.out.println("My Min Int Value Is:" + my_min_int_value);
+        System.out.println("My Max Int Value Is:" + my_max_int_value);
+
+        byte my_min_byte_value = Byte.MIN_VALUE;
+        byte my_max_byte_value = Byte.MAX_VALUE;
+
+        System.out.println("My Min Byte Value Is:" + my_min_byte_value);
+        System.out.println("My Max Byte Value Is:" + my_max_byte_value);
+
+        short my_min_short_value = Short.MIN_VALUE;
+        short my_max_short_value = Short.MAX_VALUE;
+
+        System.out.println("My Min Short Value Is:" + my_min_short_value);
+        System.out.println("My Max Short Value Is:" + my_max_short_value);
+
+        long my_min_long_value = Long.MIN_VALUE;
+        long my_max_long_value = Long.MAX_VALUE;
+
+        System.out.println("My Min Long Value Is:" + my_min_long_value);
+        System.out.println("My Max Long Value Is:" + my_max_long_value);
+    }
+}
