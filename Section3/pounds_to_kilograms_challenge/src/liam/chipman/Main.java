2bf6b237934c6d114d986b844e0a6d6ac196c1a3
Liam Chipman <chipman.lian@gmail.com>
2020-06-26 18:08:52 -0600

    Primitive types video

---
+++ Section3/pounds_to_kilograms_challenge/src/liam/chipman/Main.java
@@ -0,0 +1,10 @@
+package liam.chipman;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int pounds = 125;
+	    double kilos = pounds * 0.45359237;
+        System.out.println(kilos);
+    }
+}
