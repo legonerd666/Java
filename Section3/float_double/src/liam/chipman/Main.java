86d012784d5328df857feb12815b478cd13e1df0
Liam Chipman <chipman.lian@gmail.com>
2020-06-26 18:09:10 -0600

    pounds to kilos challenge

---
+++ Section3/float_double/src/liam/chipman/Main.java
@@ -0,0 +1,27 @@
+package liam.chipman;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float my_min_float = Float.MIN_VALUE;
+        float my_max_float = Float.MAX_VALUE;
+
+        System.out.println(my_min_float);
+        System.out.println(my_max_float);
+
+        double my_min_double = Double.MIN_VALUE;
+        double my_max_double = Double.MAX_VALUE;
+
+        System.out.println(my_min_double);
+        System.out.println(my_max_double);
+
+        int my_int = 5 / 3;
+        float my_float = 5f / 3f;
+        double my_double = 5d / 3d;
+
+        System.out.println(my_int);
+        System.out.println(my_float);
+        System.out.println(my_double);
+    }
+}
