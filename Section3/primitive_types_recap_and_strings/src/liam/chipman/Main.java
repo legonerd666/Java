36401c588fb8272396cc71268a46e2b5aa8c3720
Liam Chipman <chipman.lian@gmail.com>
2020-06-26 19:09:35 -0600

    primitive types recap and strings video

---
+++ Section3/primitive_types_recap_and_strings/src/liam/chipman/Main.java
@@ -0,0 +1,19 @@
+package liam.chipman;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        String my_string = "Hello";
+        System.out.println(my_string + my_string);
+        int my_int = 100;
+        System.out.println(my_int + my_string);
+    }
+}
