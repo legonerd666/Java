f0f3845d9d10366c9dbca0e2a9ee3e208274b818
Liam Chipman <chipman.lian@gmail.com>
2020-06-26 18:52:24 -0600

    Char and boolean video

---
+++ Section3/char_boolean/src/liam/chipman/Main.java
@@ -0,0 +1,18 @@
+package liam.chipman;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char my_char = 'D';
+        char my_unicode_char = '\u0044';
+        boolean my_true_boolean = true;
+        boolean my_false_boolean = false;
+
+        System.out.println(my_char);
+        System.out.println(my_unicode_char);
+        char my_copyright_char = '\u00A9';
+        System.out.println(my_copyright_char);
+
+    }
+}
